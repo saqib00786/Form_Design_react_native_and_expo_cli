dfb9f50282c6967e6328ff8ace91bf3737b0ed86
Muhammad Saqib <37036366+saqib00786@users.noreply.github.com>
2022-08-11 11:58:37 +0500

    initial commit

--- App.js
+++ App.js
@@ -6,43 +6,9 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Form />
-      {/* <BackgroundDesign /> */}
     </View>
   );
 }
-
-// const BackgroundDesign = () => {
-
-//   return (
-
-//     <View style={{
-//       flex: 1,
-//       flexDirection: 'column',
-//       padding: 20,
-//       width: '100%',
-//     }}>
-
-//       <View style={[styles.content,{ flex: .10, backgroundColor: '#7fffd4'}]}>
-//         <Text style={[styles.textContainer]}>
-//           View 1
-//         </Text>
-//       </View>
-//       <View style={[styles.content,{ flex: .30, backgroundColor: '#faebd7'}]}>
-//         <Text style={[styles.textContainer]}>
-//           View 2
-//         </Text>
-//       </View>
-//       <View style={[styles.content,{ flex: .60, backgroundColor: '#f0ffff' }]}>
-//         <Text style={[styles.textContainer]}>
-//           View 3
-//         </Text>
-//       </View>
-
-
-
-//     </View>
-//   );
-// }
 
 const styles = StyleSheet.create({
   container: {
